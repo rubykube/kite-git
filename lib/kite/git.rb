6848da1171bcac5cd31585752b7a7990b052cbe0
vshatravenko <vshatravenko@heliostech.fr>
2019-05-28 23:01:33 +0300

    Run bundle gem .

---
+++ lib/kite/git.rb
@@ -0,0 +1,8 @@
+require "kite/git/version"
+
+module Kite
+  module Git
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/kite/git_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Kite::Git do
+  it "has a version number" do
+    expect(Kite::Git::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
